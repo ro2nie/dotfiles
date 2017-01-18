21416de7e78ab630804093cc057065c523ccbba9
Ronnie Winter <ro2nie@gmail.com>
2017-01-18 18:15:39 +0000

    Initial commit i3

---
+++ large_monitor_on.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VIRTUAL1 --off --output LVDS1 --off --output LVDS-0 --off --output VGA1 --off --output DP-5 --off --output DP-4 --off --output DP-3 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-2 --off --output DP-1 --off --output DP-0 --off
